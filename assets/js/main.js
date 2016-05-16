965ed0f9bd989ba63e12769a40c19683d81cff84
SerhiyGuivan <serhiy.guivan@gmail.com>
2016-05-16 17:19:27 +0300

    edit js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,5 +4,16 @@ var bsCollapse = require('bootstrap-styl/js/collapse'),
     bsTransition = require('bootstrap-styl/js/transition');
 
 (function($){
+    var $networksItems = $('.box__items--networks');
+    if($networksItems.length){
+        function changeIcon(e) {
+            $(e.target)
+                .parents('.item--networks')
+                .find('.glyphicon')
+                .toggleClass('glyphicon-minus', 'glyphicon-plus')
 
+        }
+        $networksItems.on('show.bs.collapse',changeIcon);
+        $networksItems.on('hide.bs.collapse',changeIcon);
+    }
 })(jQuery);
